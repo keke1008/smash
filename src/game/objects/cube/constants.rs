d791fc00e13c4788e789085e272791984e439e01
keke1008 <keke2222337@gmail.com>
2023-01-10 01:26:18 +0900

    refactor(game): change cube 3d model

--- src/game/objects/cube/constants.rs
+++ src/game/objects/cube/constants.rs
@@ -7,6 +7,6 @@ pub(super) const FRICTION: f32 = 0.5;
 pub(super) const MOVEMENT_PER_SEC: f32 = 8.0;
 
 pub(super) const HALF_SIZE: f32 = CUBE_WIDTH / 2.0;
-pub(super) const MODEL_SCALE: f32 = CUBE_WIDTH / 2.0;
+pub(super) const MODEL_SCALE: f32 = 1.0;
 
 pub(super) const MIN_HEIGHT: f32 = -100.0;
